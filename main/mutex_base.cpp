1d11c70e8e9c3f0d2ad0a1215de469cdd73a14c2
Gabe R. <9311953+Marsfan@users.noreply.github.com>
2023-12-31 17:28:06 -0600

    New base class for classes that need a mutex.

---
+++ main/mutex_base.cpp
@@ -0,0 +1,48 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https: //mozilla.org/MPL/2.0/.
+ */
+
+#include "mutex_base.h"
+
+#include <esp_err.h>
+#include <esp_log.h>
+
+#include "common.h"
+
+MutexBase::MutexBase() {
+    // TODO: Use static version?
+    this->mutex = xSemaphoreCreateMutex();
+    if (this->mutex == NULL) {
+        ESP_LOGE("mutex_base", "Failed to create mutex");
+        ESP_ERROR_CHECK(ESP_FAIL);
+    }
+}
+
+MutexBase::MutexBase(const TickType_t wait_time) {
+    this->wait_time = wait_time;
+    // TODO: Use static version?
+    this->mutex = xSemaphoreCreateMutex();
+    if (this->mutex == NULL) {
+        ESP_LOGE("mutex_base", "Failed to create mutex");
+        ESP_ERROR_CHECK(ESP_FAIL);
+    }
+}
+
+bool MutexBase::take_mutex(const char *const tag, const char *const func) {
+    bool result = true;
+    if (xSemaphoreTake(this->mutex, this->wait_time) != pdTRUE) {
+        // TODO: Log task name too?
+        ESP_LOGE(tag, "Failed to take mutex in function %s", func);
+        result = false;
+    }
+    return result;
+}
+
+void MutexBase::release_mutex(const char *const tag, const char *const func) {
+    if (xSemaphoreGive(this->mutex) != pdTRUE) {
+        // TODO: Log task name too?
+        ESP_LOGE(tag, "Failed to release the mutex in function %s", func);
+    }
+}
---
+++ main/mutex_base.h
@@ -0,0 +1,46 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https: //mozilla.org/MPL/2.0/.
+ */
+/// @brief Base class for creating a class that uses a single mutex.
+/// @details Other classses are subclassed from this to avoid repetition.
+#ifndef MUTEX_BASE_H
+#define MUTEX_BASE_H
+
+#include <semaphore.h>
+
+class MutexBase {
+   protected:
+    /// @brief instantiate the class.
+    /// @details When instantiated this way, the wait time will be the
+    /// MAX_MUTEX_WAIT_TICKS defined in "common.h"
+    MutexBase(void);
+
+    /// @brief Instantiate the class and specify the max allowed wait time to
+    /// obtain the mutex.
+    /// @param wait_time The maximum allowed amount of time to wait for the
+    /// mutex.
+    MutexBase(const TickType_t wait_time);
+
+    /// @brief Take the classes's mutex.
+    /// @param tag Tag to use when logging a failed attempt at reading the
+    /// mutex.
+    /// @param func The name of the calling function.
+    /// @return Boolean indicting if the mutex was successfully taken.
+    bool take_mutex(const char *const tag, const char *const func);
+
+    /// @brief Release classes's mutex.
+    /// @param tag Tag to use when logging a failed attempt at reading the
+    /// mutex.
+    /// @param func The name of the calling function.'
+    void release_mutex(const char *const tag, const char *const func);
+
+   private:
+    /// @brief  Mutex to ensure task safety.
+    SemaphoreHandle_t mutex;
+
+    /// @brief Time to wait to obtain the mutex.
+    TickType_t wait_time;
+};
+#endif  // MUTEX_BASE_H
